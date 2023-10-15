b81801280e98ea2c8676b782bf29fe74da6d333b
soyezequiel <alcayaga147147@gmail.com>
2023-10-15 11:44:48 -0300

    se agrego control de volumen

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
 const OBSWebSocket = require('obs-websocket-js');
 // Crea una nueva instancia del cliente de Discord
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
 });
 // Crea una nueva instancia de OBS WebSocket
 const obs = new OBSWebSocket();
@@ -15,13 +15,13 @@ const obs = new OBSWebSocket();
 const token = process.env.DISCORD_TOKEN;
 // Función para conectar con OBS
 async function connectToObs() {
-    try {
-        await obs.connect({ address: 'localhost:4444' });
-        console.log('Conectado a OBS');
-    } catch (err) {
-        console.error('No se pudo conectar a OBS:', err);
-        setTimeout(connectToObs, 5000);
-    }
+  try {
+    await obs.connect({ address: 'localhost:4444' });
+    console.log('Conectado a OBS');
+  } catch (err) {
+    console.error('No se pudo conectar a OBS:', err);
+    setTimeout(connectToObs, 5000);
+  }
 }
 // Llama a la función para conectarse a OBS
 connectToObs();
@@ -52,6 +52,18 @@ client.once('ready', async () => {
         }
       ]
     }
+    ,{
+      name: 'setvolume',
+      description: 'Ajusta el volumen de la fuente "w2g nav"',
+      options: [
+        {
+          name: 'volume',
+          type: 4,  // Tipo INTEGER
+          description: 'Valor del volumen (0 a 100)',
+          required: true
+        }
+      ]
+    }
   ];
   // Crea una nueva instancia REST para hacer peticiones a la API de Discord
   const rest = new REST({ version: '9' }).setToken(token);
@@ -68,9 +80,16 @@ client.once('ready', async () => {
     console.error(error);
   }
 });
+
+
+
+
+
+
+
 // Evento que se dispara cuando se crea una interacción (ejemplo: un usuario utiliza un comando de barra inclinada)
 client.on('interactionCreate', async interaction => {
-    // Verifica si la interacción es un comando
+  // Verifica si la interacción es un comando
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
@@ -105,7 +124,39 @@ client.on('interactionCreate', async interaction => {
       await interaction.reply('Error al cambiar la escena, asegúrate de que el nombre de la escena es correcto.');
     }
   }
-});
-// Conecta el bot a Discord usando el token
+// Comando para ajustar el volumen de "w2g nav"
+if (interaction.commandName === 'setvolume') {
+  const volume = interaction.options.getInteger('volume');
+  console.log(`Volume received from Discord: ${volume}`);  // Log the received volume
 
+  // Ensure the volume is within the allowed range
+  if (volume < 0 || volume > 100) {
+      await interaction.reply('El volumen debe estar entre 0 y 100.');
+      return;
+  }
+
+  const volumeScaled = volume / 100.0;  // Scale the volume to a 0.0 - 1.0 range
+  console.log(`Volume scaled for OBS: ${volumeScaled}`);  // Log the scaled volume
+
+  try {
+      await obs.send('SetVolume', {
+          source: 'musica',  // Updated source name
+          volume: volumeScaled,
+          useDecibel: false  // This will use a 0.0 to 1.0 scale for volume
+      });
+      await interaction.reply(`Volumen ajustado a: ${volume}%`);
+  } catch (error) {
+      console.error('Error setting volume:', error);  // Log any errors
+      await interaction.reply('Error al ajustar el volumen.');
+  }
+}
+
+});
+
+
+
+// Conecta el bot a Discord usando el token
 client.login(token);
+
+
+
